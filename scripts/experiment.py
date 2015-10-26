42223704008bc3f02f0ad8a12283050c6a680662
Milan Pavlik <pavlik.mil@gmail.com>
2015-10-26 10:12:28 +0000

    Log

--- scripts/experiment.py
+++ scripts/experiment.py
@@ -3,6 +3,7 @@ from pssh import ParallelSSHClient
 hosts = ['nsl200', 'nsl201', 'nsl202', 'nsl203', 'nsl204', 'nsl205', 'nsl206', 'nsl207']
 
 client = ParallelSSHClient(hosts)
+client.enable_host_logger()
 
 output = client.run_command('ls -ltr')
 
