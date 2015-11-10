4fee87a59570e61aa0bacc95d748856c814748d9
Milan Pavlik <pavlik.mil@gmail.com>
2015-11-10 00:08:09 +0000

    Fix typo

--- scripts/parsers/latency.py
+++ scripts/parsers/latency.py
@@ -9,13 +9,13 @@ class LatencyParser(object):
         self.content = content
 
     def get_average_latency(self):
-        first_index = self.content.find(self.TOTALS_DELIM)
+        first_index = self.content.index(self.TOTALS_DELIM)
         totals = self.content[first_index+3]
         latency = totals.split()[4]
         return latency
 
     def get_99th_latency(self):
-        gets_start = self.content.find(self.GETS_DELIM)
+        gets_start = self.content.index(self.GETS_DELIM)
         gets = self.content[gets_start+1:]
 
         for get in gets:
