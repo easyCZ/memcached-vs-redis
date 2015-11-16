46f51b0522ed20529e22782812da93dbd075412b
Milan Pavlik <pavlik.mil@gmail.com>
2015-11-16 13:57:12 +0000

    config for multiple isntances

--- results/aggregate.py
+++ results/aggregate.py
@@ -3,14 +3,16 @@ import sys
 lines = []
 
 # Should receive it sorted
-print('cpu, latency, 99th')
+print('usr, sys, cpu, latency, 99th')
 for line in sys.stdin:
-    cpu, latency, last = line.strip().split(',')
+    cpu, usr, sys, latency, last = line.strip().split(',')
 
-    lines.append((float(cpu), float(latency), float(last)))
+    lines.append((float(cpu), float(usr), float(sys), float(latency), float(last)))
     if len(lines) == 3:
-        avg_cpu = sum([c for c, l, q in lines]) / 3
-        avg_latency = sum([l for c, l, q in lines]) / 3
-        avg_last = sum([q for c, l, q in lines]) / 3
-        print(', '.join([str(avg_cpu), str(avg_latency), str(avg_last)]))
+        avg_cpu = sum([c for c, usr, sys, l, q in lines]) / 3
+        avg_latency = sum([l for c, usr, sys, l, q in lines]) / 3
+        avg_last = sum([q for c, usr, sys, l, q in lines]) / 3
+        avg_usr = sum([usr for c, usr, sys, l, q in lines]) / 3
+        avg_sys = sum([sys for c, usr, sys, l, q in lines]) / 3
+        print(', '.join([str(avg_usr), str(avg_sys), str(avg_cpu), str(avg_latency), str(avg_last)]))
         lines = []
