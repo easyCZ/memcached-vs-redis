d93d4d0a53b0f6996ab361d067b324eafc226861
Milan Pavlik <pavlik.mil@gmail.com>
2015-12-01 22:48:22 +0000

    Add play

---
+++ benchmark/play.py
@@ -0,0 +1,7 @@
+from clients import Clients
+from server import Server
+
+s = Server('memcached', '-d -p 11120 -m 1024', 11120, 4)
+s.start_cache()
+
+c = Clients('memcached', '-s nsl200 -p 11123 --test-time=30 -c 1 -t 1 -P memcache_binary', 11120, 4)
