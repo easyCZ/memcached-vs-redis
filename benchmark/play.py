a2d5c325fcde1d6418603d398fc5e51e65180155
Milan Pavlik <pavlik.mil@gmail.com>
2015-12-02 14:25:17 +0000

    Play

--- benchmark/play.py
+++ benchmark/play.py
@@ -1,4 +1,5 @@
 from clients import Clients
+from parsers.cpu import CPUParser
 from parsers.memtier import MemtierResultsParser
 from server import Server
 
@@ -6,6 +7,9 @@ s = Server('memcached', '-d -p 11120 -m 1024', 11120, 4)
 s.start_cache()
 
 c = Clients('memcached', '-s nsl200 -p 11123 --test-time=30 -c 1 -t 1 -P memcache_binary', 11120, 4)
+
+cpu = s.log_cpu()
+cpu_parser = CPUParser()
 results = c.run_memtier()
 
 parser = MemtierResultsParser(results)
