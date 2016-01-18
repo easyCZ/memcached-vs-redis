9b270298d08fb1052dec3d371a8a982b92a7abde
Milan Pavlik <pavlik.mil@gmail.com>
2016-01-18 11:03:18 +0000

    Shift to LaTeX

--- dissertation/compile.sh
+++ dissertation/compile.sh
@@ -2,3 +2,9 @@ pandoc *.md \
     -V geometry:margin=3cm \
     --number-sections \
     -o dissertation.pdf
+
+# pandoc \
+#     -s dissertation.tex \
+#     -o dissertation.pdf \
+#     -V geometry:margin=3cm
+    # --number-sections
