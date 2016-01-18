10caa16bbb79f3f1c91a3992c7eb809070f00e90
Milan Pavlik <pavlik.mil@gmail.com>
2016-01-18 14:09:20 +0000

    Update compile script

--- dissertation/compile.sh
+++ dissertation/compile.sh
@@ -1,10 +1,1 @@
-pandoc *.md \
-    -V geometry:margin=3cm \
-    --number-sections \
-    -o dissertation.pdf
-
-# pandoc \
-#     -s dissertation.tex \
-#     -o dissertation.pdf \
-#     -V geometry:margin=3cm
-    # --number-sections
+pdflatex report.tex
