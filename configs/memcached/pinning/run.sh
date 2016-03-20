1465b27d4be1b4ca490355fcc7026b0fcffccb14
Milan Pavlik <pavlik.mil@gmail.com>
2016-03-20 15:39:29 +0000

    Add pinning conf

---
+++ configs/memcached/pinning/run.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+
+DIR=pinning
+
+for i in {1..12}
+do
+    python ../../../benchmark/main.py \
+        -type=memcached \
+        -server-conf=./memcached.$i.conf \
+        -memtier-conf=./memtier.memcached.conf \
+        -output=../../../results/memcached/$DIR/$i/ \
+        -base-port=11120 \
+        -instances=1 \
+        -duration=30
+done
+
+git add ../../../
+git commit -m "Memcached Thread Pinning Results"
+git push
