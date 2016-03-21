95def288c6d7a83aac87c1c5dcf367d3691ec21c
Milan Pavlik <pavlik.mil@gmail.com>
2016-03-21 23:15:36 +0000

    Add config 4

---
+++ configs/redis/instances4/run.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+DIR=instances4
+
+for i in {1..9}
+do
+    python ../../../benchmark/main.py \
+        -type=redis \
+        -server-conf=./redis.conf \
+        -memtier-conf=./memtier.redis.$i.conf \
+        -output=../../../results/redis/$DIR/$i/ \
+        -base-port=11120 \
+        -instances=$i \
+        -duration=30
+done
