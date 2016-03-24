b923331456d3fb895d42b646351521a1d2f955d5
Milan Pavlik <pavlik.mil@gmail.com>
2016-03-24 21:12:54 +0000

    Remove direct ssh

--- configs/set-irq-affinitiy.sh
+++ configs/set-irq-affinitiy.sh
@@ -1,9 +1,5 @@
-ssh nsl200 << 'ENDSSH'
+echo 0-5 | sudo tee /proc/irq/81/smp_affinity_list;
 
-    echo 0-5 | sudo tee /proc/irq/81/smp_affinity_list;
-
-    for i in $(seq 82 1 87); do
-        echo $(($i % 6)) | sudo tee /proc/irq/$i/smp_affinity_list;
-    done
-
-ENDSSH
+for i in $(seq 82 1 87); do
+    echo $(($i % 6)) | sudo tee /proc/irq/$i/smp_affinity_list;
+done
