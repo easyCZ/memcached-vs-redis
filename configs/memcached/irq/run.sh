d3e6c84fd802e49af8e96e730f47c97e4ed430a0
Milan Pavlik <pavlik.mil@gmail.com>
2016-03-25 01:46:29 +0000

    Add irq

---
+++ configs/memcached/irq/run.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+DIR=irq
+
+for i in {1..10}
+do
+    python ../../../benchmark/main.py \
+        -type=memcached \
+        -server-conf=./memcached.$i.conf \
+        -memtier-conf=./memtier.memcached.conf \
+        -output=../../../results/memcached/$DIR/$i/ \
+        -base-port=11120 \
+        -instances=1 \
+        -duration=30
+done
