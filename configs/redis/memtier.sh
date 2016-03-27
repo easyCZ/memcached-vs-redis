beb5a46e2ff362447e9f7c2fd5c3f204df992d3d
Milan Pavlik <pavlik.mil@gmail.com>
2016-03-27 21:05:17 +0100

    Add redis baseline conf

--- configs/redis/memtier.sh
+++ configs/redis/memtier.sh
@@ -1,19 +1,22 @@
 #!/bin/bash
 
 LABEL=baseline
+DATA_SIZE=64
 mkdir $LABEL
 
-for i in {1..20}
+for i in {1..14}
 do
     echo "-s nsl200
 -p 11120
---test-time=30
+--test-time=60
 -c $i
 -t 2
 -P redis
 --random-data
+--data-size=64
 --key-minimum=1
---key-maximum=100000
-" > $LABEL/memtier.redis.$i.conf
+--key-maximum=10000000" > $LABEL/memtier.redis.$i.conf
+
+DATA_SIZE=$(($DATA_SIZE * 2))
 
 done
--- configs/redis/redis.sh
+++ configs/redis/redis.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
 
-LABEL=default
+LABEL=baseline
 mkdir $LABEL
 
-for i in {1..12}
+for i in {1..10}
 do
     echo "--p 11120" > $LABEL/redis.$(($i + 1)).conf
 
