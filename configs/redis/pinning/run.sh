d3317179107af09ea09fef8a8190720b16639463
Milan Pavlik <pavlik.mil@gmail.com>
2016-03-28 01:06:26 +0100

    Add pinning

--- configs/redis/pinning/run.sh
+++ configs/redis/pinning/run.sh
@@ -1,15 +1,43 @@
 #!/bin/bash
 
-DIR=pinning
+DIR=irq
 
-for i in {1..9}
-do
-    python ../../../benchmark/main.py \
-        -type=redis \
-        -server-conf=./redis.conf \
-        -memtier-conf=./memtier.redis.$i.conf \
-        -output=../../../results/redis/$DIR/$i/ \
-        -base-port=11120 \
-        -instances=$i \
-        -duration=30
-done
+python ../../../benchmark/main.py \
+    -type=redis \
+    -server-conf=./redis.1.conf \
+    -memtier-conf=./memtier.redis.1.conf \
+    -output=../../../results/redis/irq/1/ \
+    -base-port=11120 \
+    -instances=1 \
+    -duration=30 \
+    -pin=true
+
+python ../../../benchmark/main.py \
+    -type=redis \
+    -server-conf=./redis.2.conf \
+    -memtier-conf=./memtier.redis.2.conf \
+    -output=../../../results/redis/irq/2/ \
+    -base-port=11120 \
+    -instances=2 \
+    -duration=30 \
+    -pin=true
+
+python ../../../benchmark/main.py \
+    -type=redis \
+    -server-conf=./redis.3.conf \
+    -memtier-conf=./memtier.redis.3.conf \
+    -output=../../../results/redis/irq/3/ \
+    -base-port=11120 \
+    -instances=3 \
+    -duration=30 \
+    -pin=true
+
+python ../../../benchmark/main.py \
+    -type=redis \
+    -server-conf=./redis.4.conf \
+    -memtier-conf=./memtier.redis.4.conf \
+    -output=../../../results/redis/irq/4/ \
+    -base-port=11120 \
+    -instances=6 \
+    -duration=30 \
+    -pin=true
