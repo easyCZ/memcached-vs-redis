da6b7637450af5e0911b924ffbcf78cc533383e4
Milan Pavlik <pavlik.mil@gmail.com>
2016-03-28 01:33:43 +0100

    Pinning

--- configs/redis/pinning/run.sh
+++ configs/redis/pinning/run.sh
@@ -1,12 +1,12 @@
 #!/bin/bash
 
-DIR=irq
+DIR=pinning
 
 python ../../../benchmark/main.py \
     -type=redis \
     -server-conf=./redis.1.conf \
     -memtier-conf=./memtier.redis.1.conf \
-    -output=../../../results/redis/irq/1/ \
+    -output=../../../results/redis/$DIR/1/ \
     -base-port=11120 \
     -instances=1 \
     -duration=30 \
@@ -16,7 +16,7 @@ python ../../../benchmark/main.py \
     -type=redis \
     -server-conf=./redis.2.conf \
     -memtier-conf=./memtier.redis.2.conf \
-    -output=../../../results/redis/irq/2/ \
+    -output=../../../results/redis/$DIR/2/ \
     -base-port=11120 \
     -instances=2 \
     -duration=30 \
@@ -26,7 +26,7 @@ python ../../../benchmark/main.py \
     -type=redis \
     -server-conf=./redis.3.conf \
     -memtier-conf=./memtier.redis.3.conf \
-    -output=../../../results/redis/irq/3/ \
+    -output=../../../results/redis/$DIR/3/ \
     -base-port=11120 \
     -instances=3 \
     -duration=30 \
@@ -36,7 +36,7 @@ python ../../../benchmark/main.py \
     -type=redis \
     -server-conf=./redis.4.conf \
     -memtier-conf=./memtier.redis.4.conf \
-    -output=../../../results/redis/irq/4/ \
+    -output=../../../results/redis/$DIR/4/ \
     -base-port=11120 \
     -instances=6 \
     -duration=30 \
